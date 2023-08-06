1e430d001adaeab3195a44e22f9e34b326d04bfe
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-08-06 10:53:52 +0200

    added a cell

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
 }
 
---
+++ GoalPost/Views/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Maryna Bolotska on 06/08/23.
+//
+
+import UIKit
+
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
