084b8254eba7f5757d6ee0ce334468775a0185f9
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-08-07 16:18:22 +0200

    added cell

---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Maryna Bolotska on 07/08/23.
+//
+
+import Foundation
+
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
