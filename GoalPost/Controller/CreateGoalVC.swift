b3a038392856417aa6dee75f66cb401a89ac27c1
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-08-08 16:53:41 +0200

    added extensions

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,8 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Maryna Bolotska on 08/08/23.
+//
+
+import Foundation
---
+++ GoalPost/Extensions/UIViewExt.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewExt.swift
+//  GoalPost
+//
+//  Created by Maryna Bolotska on 08/08/23.
+//
+
+import Foundation
